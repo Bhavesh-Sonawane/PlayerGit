1040950f0bdf9ec380a62ff86f70e0c732957c5c
Sonawane <850058667@genpact.com>
2022-02-25 17:59:34 +0530

    Second Commit

---
+++ Models/Player.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebAppGitExaple.Models
+{
+    public class Player
+    {
+        public int PId { get; set; }
+        public string PName { get; set; }
+        public string PTeam { get; set; }
+    
+    }
+}

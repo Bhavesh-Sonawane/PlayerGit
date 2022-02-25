e0195705b73fd275d0e68d75934fe8146d780d78
Sonawane <850058667@genpact.com>
2022-02-25 18:05:33 +0530

    Third commit

---
+++ Controllers/PlayersController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebAppGitExaple.Models;
+
+namespace WebAppGitExaple.Controllers
+{
+    public class PlayersController : Controller
+    {
+        // GET: Players
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Display()
+        {
+            List<Player> players = new List<Player>()
+            {
+                new Player{PId=1,PName="Ravi",PTeam="CSK"},
+                new Player{PId=2,PName="Dinesh",PTeam="Mi"},
+                new Player{PId=3,PName="Pramod",PTeam="Delhi"}
+            };
+            return View();
+        }
+    }
+}
